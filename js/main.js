7facdeb1c995f887154e3d397940725202768f78
ameliakrisnawedding <79979718+ameliakrisnawedding@users.noreply.github.com>
2021-03-16 01:43:53 +0700

    update main.js
    update date countdown

--- js/main.js
+++ js/main.js
@@ -232,7 +232,7 @@
   };
 
   // Set the date we're counting down to
-  var countDownDate = new Date("March 14, 2021 13:00:00").getTime();
+  var countDownDate = new Date("March 16, 2021 01:45:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
